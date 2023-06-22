Stashing. Fixed exploding gradient error in transformer network.

src/Networks.py
@@ -130,6 +130,8 @@ class ValueNetworkTransformer(nn.Module):
         # Embedding layer for older states
         self.embedding = nn.Linear(self.state_size, hidden_size).to(device)
 
+        self.positional_encodings = self.create_positional_encodings(stack_size - 1, hidden_size).to(device)
+
         # Transformer encoder layers
         encoder_layers = nn.TransformerEncoderLayer(d_model=hidden_size, nhead=nhead, batch_first=True).to(device)
         self.transformer_encoder = nn.TransformerEncoder(encoder_layers, num_layers).to(device)
@@ -148,6 +150,24 @@ class ValueNetworkTransformer(nn.Module):
 
         self.apply(self.init_weights)
         self.to(self.device)
+
+    @staticmethod
+    def create_positional_encodings(seq_len: int, d_model: int):
+        """Creates positional encodings for the Transformer model.
+
+        Args:
+            seq_len: The sequence length.
+            d_model: The dimension of the embeddings (i.e., model dimension).
+            batch_size: The batch size.
+
+        Returns:
+            A tensor containing the positional encodings.
+        """
+        pos = torch.arange(seq_len, dtype=torch.float32).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model))
+        pos_enc = pos * div_term
+        pos_enc = torch.stack([torch.sin(pos_enc), torch.cos(pos_enc)], dim=-1).view(seq_len, d_model)
+        return pos_enc
 
     def init_weights(self, m):
         if type(m) == nn.Linear:
@@ -174,39 +194,47 @@ class ValueNetworkTransformer(nn.Module):
         state = state.reshape(num_envs * num_samples, self.stack_size, self.state_size)
         recent_state, older_states = state[:, -1, :], state[:, :-1, :]
 
-        # Flatten the older states back into 2D form for the embedding layer
-        older_states = older_states.reshape(num_envs * num_samples * (self.stack_size - 1), self.state_size)
+         # Flatten the older states back into 2D form for the embedding layer
+        older_states = older_states.reshape(num_envs * num_samples, (self.stack_size - 1), self.state_size)
+
+        # Move tensors to the specified device
         older_states = older_states.to(self.device)
         recent_state = recent_state.to(self.device)
 
-        # Create a mask to ignore zero-padded states in the transformer encoder
-        non_zero_mask = torch.all(older_states == 0, dim=-1)
-        mask = non_zero_mask.reshape(num_envs * num_samples, self.stack_size - 1)
+        
 
         # Embed the older states and reshape them back into sequence form
         embedded_states = self.embedding(older_states)
-        embedded_states = embedded_states.reshape(num_envs * num_samples, self.stack_size - 1, self.hidden_size)
 
-        # Create the key_padding_mask by inverting the mask and converting it to a byte tensor
-        key_padding_mask = mask.to(torch.bool).to(self.device)
+        # Add positional encodings to the older states
+        embedded_states += self.positional_encodings
 
-        # Pass the older states through the transformer encoder only if there are non-zero states
-        if non_zero_mask.any():
-            transformer_output = self.transformer_encoder(embedded_states, src_key_padding_mask=key_padding_mask)
-        else:
-            transformer_output = torch.zeros_like(embedded_states)  # Output zeros if all older states are zero
+        # # Create a mask to ignore zero-padded states in the transformer encoder
+        # mask = torch.all(older_states == 0, dim=2)
 
-        # Handle NaN values in the transformer_output (When older_states are all empty)
-        transformer_output = torch.where(torch.isnan(transformer_output), torch.zeros_like(transformer_output), transformer_output)
+        # # Create the key_padding_mask by inverting the mask and converting it to a byte tensor
+        # key_padding_mask = mask.to(torch.bool).to(self.device)
+
+        # # Pass the older states through the transformer encoder only if there are non-zero states
+        # if key_padding_mask.any():
+        #     transformer_output = self.transformer_encoder(embedded_states, src_key_padding_mask=key_padding_mask)
+        # else:
+        #     transformer_output = torch.zeros_like(embedded_states)  # Output zeros if all older states are zero
+
+        # # Handle NaN values in the transformer_output (When older_states are all empty)
+        # transformer_output = torch.where(torch.isnan(transformer_output), torch.zeros_like(transformer_output), transformer_output)
+
+        # Pass the older states through the transformer encoder
+        transformer_output = self.transformer_encoder(embedded_states)
 
         # Process the most recent state through the linear layer
         linear_output = self.linear(recent_state)
 
         # Apply max pooling to the transformer output
-        pooled_output, _ = torch.max(transformer_output, dim=1)
+        pooled_output = torch.mean(transformer_output, dim=1)
 
         # Combine the pooled transformer output and linear output
-        combined_output = torch.cat((pooled_output, linear_output), dim=-1)
+        combined_output = F.leaky_relu(torch.cat((pooled_output, linear_output), dim=-1))
 
         # Compute the value predictions
         value = self.value_net(combined_output).view(num_envs, num_samples, 1)

src/Policies.py
@@ -3,6 +3,7 @@ from torch import nn
 from torch.nn import functional as F
 from Networks import StatefulTransformer
 from typing import Tuple
+import math
 
 class DiscreteGradientPolicy(nn.Module):
     def __init__(self, in_features : int, out_features : int, hidden_size : int, device : torch.device = torch.device("cpu")):
@@ -90,6 +91,8 @@ class GaussianGradientTransformerPolicy(nn.Module):
 
         self.embedding = nn.Linear(self.state_size, hidden_size).to(device)
 
+        self.positional_encodings = self.create_positional_encodings(stack_size - 1, hidden_size).to(device)
+
         encoder_layers = nn.TransformerEncoderLayer(d_model=hidden_size, nhead=nhead, batch_first=True).to(device)
         self.transformer_encoder = nn.TransformerEncoder(encoder_layers, num_layers).to(device)
 
@@ -109,11 +112,31 @@ class GaussianGradientTransformerPolicy(nn.Module):
         self.eps = 1e-8
         self.min_std_value = 1e-5
 
+    @staticmethod
+    def create_positional_encodings(seq_len: int, d_model: int):
+        """Creates positional encodings for the Transformer model.
+
+        Args:
+            seq_len: The sequence length.
+            d_model: The dimension of the embeddings (i.e., model dimension).
+            batch_size: The batch size.
+
+        Returns:
+            A tensor containing the positional encodings.
+        """
+        pos = torch.arange(seq_len, dtype=torch.float32).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model))
+        pos_enc = pos * div_term
+        pos_enc = torch.stack([torch.sin(pos_enc), torch.cos(pos_enc)], dim=-1).view(seq_len, d_model)
+        return pos_enc
+
+
     def init_weights(self, m):
         # Weight initialization function to be applied to each layer
-        if type(m) == nn.Linear:
-            nn.init.kaiming_normal_(m.weight, a=0.01)
-            m.bias.data.fill_(0.0)
+        pass
+        # if type(m) == nn.Linear:
+        #     nn.init.kaiming_normal_(m.weight, a=0.01)
+        #     m.bias.data.fill_(0.0)
 
     def forward(self, state: torch.Tensor):
         """
@@ -140,42 +163,46 @@ class GaussianGradientTransformerPolicy(nn.Module):
         recent_state, older_states = state[:, -1, :], state[:, :-1, :]
 
         # Flatten the older states back into 2D form for the embedding layer
-        older_states = older_states.reshape(num_envs * num_samples * (self.stack_size - 1), self.state_size)
+        older_states = older_states.reshape(num_envs * num_samples, (self.stack_size - 1), self.state_size)
 
         # Move tensors to the specified device
         older_states = older_states.to(self.device)
         recent_state = recent_state.to(self.device)
 
-        # Create a mask to ignore zero-padded states in the transformer encoder
-        non_zero_mask = torch.all(older_states == 0, dim=-1)
-        mask = non_zero_mask.reshape(num_envs * num_samples, self.stack_size - 1)
-
         # Embed the older states and reshape them back into sequence form
         embedded_states = self.embedding(older_states)
-        embedded_states = embedded_states.reshape(num_envs * num_samples, self.stack_size - 1, self.hidden_size)
 
-        # Create the key_padding_mask by inverting the mask and converting it to a byte tensor
-        key_padding_mask = mask.to(torch.bool).to(self.device)
+        # Add positional encodings to the older states
+        embedded_states += self.positional_encodings
 
-        # Pass the older states through the transformer encoder only if there are non-zero states
-        if non_zero_mask.any():
-            transformer_output = self.transformer_encoder(embedded_states, src_key_padding_mask=key_padding_mask)
-        else:
-            transformer_output = torch.zeros_like(embedded_states)  # Output zeros if all older states are zero
+        # # Create a mask to ignore zero-padded states in the transformer encoder
+        # mask = torch.all(older_states == 0, dim=2)
 
-        # Handle NaN values in the transformer_output (When older_states are all empty)
-        transformer_output = torch.where(torch.isnan(transformer_output), torch.zeros_like(transformer_output), transformer_output)
+        # # Create the key_padding_mask by inverting the mask and converting it to a byte tensor
+        # key_padding_mask = mask.to(torch.bool).to(self.device)
+
+        # # Pass the older states through the transformer encoder only if there are non-zero states
+        # if key_padding_mask.any():
+        #     transformer_output = self.transformer_encoder(embedded_states, src_key_padding_mask=key_padding_mask)
+        # else:
+        #     transformer_output = torch.zeros_like(embedded_states)  # Output zeros if all older states are zero
+
+        # # Handle NaN values in the transformer_output (When older_states are all empty)
+        # transformer_output = F.leaky_relu(torch.where(torch.isnan(transformer_output), torch.zeros_like(transformer_output), transformer_output))
+
+        # Pass the older states through the transformer encoder
+        transformer_output = self.transformer_encoder(embedded_states)
 
         # Process the most recent state through the linear layer
         linear_output = self.linear(recent_state)
 
         # Combine the outputs of the transformer encoder and the linear layer
-        pooled_output, _ = torch.max(transformer_output, dim=1)
-        combined_output = torch.cat((pooled_output, linear_output), dim=-1)
+        pooled_output = torch.mean(transformer_output, dim=1)
+        combined_output = F.leaky_relu(torch.cat((pooled_output, linear_output), dim=-1))
 
         # Compute the mean and standard deviation
-        means = self.mean(combined_output).view(num_envs, num_samples, self.out_features)
-        stds = self.std(combined_output).view(num_envs, num_samples, self.out_features)
+        means = self.mean(combined_output).view(num_envs, num_samples, self.out_features) + self.eps
+        stds = self.std(combined_output).view(num_envs, num_samples, self.out_features) + self.eps
         stds = torch.clamp(stds, min=self.min_std_value)  # Clamp the standard deviation to avoid values too close to 0
 
         return means, stds

src/brax_to_gymnasium.py
@@ -42,7 +42,64 @@ def convert_brax_to_gym(name: str, frame_stack: bool = True, stack_size: int = 1
 
     return env
 
+
 class BraxFrameStack(gym.Wrapper):
+    def __init__(self, env: gym.Env, stack_size: int, device: torch.device = torch.device("cpu")):
+        super().__init__(env)
+        self.stack_size = stack_size
+        self.num_envs = env.num_envs
+        self.device = device
+
+        self.stacks = [torch.zeros((self.stack_size, self.env.observation_space.shape[-1]), dtype=torch.float32).to(self.device) for _ in range(self.num_envs)]
+        
+        self.observation_space = gym.spaces.Box(
+            low=-np.inf, 
+            high=np.inf, 
+            shape=(self.num_envs, self.stack_size * self.env.observation_space.shape[-1]), 
+            dtype=np.float32
+        )
+
+    def reset(self):
+        obs, info = self.env.reset()
+        self.stacks = [torch.zeros((self.stack_size, self.env.observation_space.shape[-1]), dtype=torch.float32).to(self.device) for _ in range(self.num_envs)]
+        for i in range(self.num_envs):
+            self.stacks[i][-1] = obs[i, :].to(self.device)
+        return self.get_obs(), info
+
+    def step(self, action):
+        obs, reward, done, truncs, info = self.env.step(action)
+
+        # Create a list to hold the information about whether each episode is done or truncated
+        reset_indices = []
+
+        for i in range(self.num_envs):
+            self.stacks[i] = torch.cat((self.stacks[i][1:], obs[i, :].unsqueeze(0).to(self.device)))
+
+            # If the episode is done or truncated, add the index to the list
+            if done[i] or truncs[i]:
+                reset_indices.append(i)
+
+        # Store the current state of the stacks
+        curr_obs = self.get_obs()
+
+        # Now, reset the stacks for the environments where the episode is done or truncated
+        for i in reset_indices:
+            self.stacks[i] = torch.zeros((self.stack_size, self.env.observation_space.shape[-1]), dtype=torch.float32).to(self.device)
+
+        return curr_obs, reward, done, truncs, info
+
+    def get_obs(self):
+        # Convert the list of stacks into a 3D tensor of shape (num_envs, stack_size, state_size)
+        obs = torch.stack(self.stacks)
+
+        # Reshape the tensor to be of shape (num_envs, stack_size * state_size)
+        obs = obs.view(self.num_envs, -1)
+
+        return obs
+
+
+
+class BraxFrameStackV1(gym.Wrapper):
     def __init__(self, env: gym.Env, stack_size: int, device: torch.device = torch.device("cpu")):
         super().__init__(env)
         self.stack_size = stack_size  # Number of frames to stack
@@ -79,9 +136,14 @@ class BraxFrameStack(gym.Wrapper):
         return self.get_obs(), reward, done, truncs, info  # Return the stacked frames, reward, done flag, truncations, and info
 
     def get_obs(self):
-        # Stack the frames together and flatten the last two dimensions
-        obs = torch.stack([torch.stack(list(frame)) for frame in self.frames]).view(self.num_envs, -1)
+        # Convert the list of stacks into a 3D tensor of shape (num_envs, stack_size, state_size)
+        obs = torch.stack(self.stacks)
+
+        # Reshape the tensor to be of shape (num_envs, stack_size * state_size)
+        obs = obs.view(self.num_envs, -1)
+
         return obs
+
 
 class GymWrapper(gym.Env):
 